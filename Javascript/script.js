6b8fa996e5764d456fb897b209c3762cae81b2ad
leonardoolia <leonardooliaa@gmail.com>
2023-12-17 18:44:23 +0100

    added message and password

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,8 +1,12 @@
-// Recuperiamo l'elemento HTML
+// Recuperiamo l'elemento HTML (1)
 const element = document.getElementById('element');
 console.log(element);
 
-//Dichiariamo le variabili e chiediamo nome, cognome, colore (2,3)
+// Prepariamo il messaggio (2)
+let message = 'La tua password è: ';
+console.log(message);
+
+//Dichiariamo le variabili e chiediamo nome, cognome, colore (3,4)
 const firstName = prompt('Qual è il tuo nome?', 'Leonardo');
 console.log(firstName);
 
@@ -14,6 +18,11 @@ console.log(favoriteColor);
 
 const passwordNumber = '21';
 console.log(passwordNumber);
+
+// Generiamo la password (6)
+
+const userPassword = firstName + secondName + favoriteColor + passwordNumber;
+console.log(userPassword);
 
 
 
