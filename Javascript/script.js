cded07258856ff8f7d0363673355ad2d3121f833
leonardoolia <leonardooliaa@gmail.com>
2023-12-17 17:55:35 +0100

    create variabili e chiesti dati all'utente

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,16 @@
-console.log('JS OK')
+//Dichiariamo le variabili e chiediamo nome, cognome, colore (1,2)
+const firstName = prompt('Qual è il tuo nome?', 'Leonardo');
+console.log(firstName);
+
+const secondName = prompt('Qual è il tuo cognome?', 'Olia');
+console.log(secondName);
+
+const favoriteColor = prompt('Qual è il tuo colore preferito?', 'Viola');
+console.log(favoriteColor);
+
+const passwordNumber = '21';
+console.log(passwordNumber);
+
+
+
+
